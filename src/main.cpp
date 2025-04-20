e5374c5c5450d0bee436de183e8a62e5d562e593
FunctionalJerk <d.hanraths@web.de>
2025-04-20 20:23:23 +0200

    create new branch for HID

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,73 @@
 #include <Arduino.h>
+#include <usb_dev.h>      // Teensy's core USB functions
+// #include "custom_usb_desc.h"     // Your custom descriptor (quotes = local first)
+// #include "custom_usb_names.h"    // Your custom names
+#include <usb_rawhid.h>
+// #include "usb_custom.h"
 
 #define numSwitches 3 // overall input switchPins connected
 #define numPots 4     // overall input potPins connected
 
+// usb_custom_class CustomHID;
+
 const uint8_t switchPins[numSwitches] = {19, 18, 17};
 const uint8_t potPins[numPots] = {23, 22, 21, 20};
-const uint8_t bitdepth = 14;
-const int tickrate = 6; // this depends on 1 / (baudrate / msg-size) * 1000 = 5.73..
+const uint8_t bitdepth = 16;
+const uint8_t numSwitchBytes = numSwitches / 8 + 1;
+const uint8_t numPaddingBytes = RAWHID_TX_SIZE - (sizeof(uint16_t) * numPots) - (sizeof(uint8_t) * numSwitchBytes);
 
-const uint8_t START_MARKER = 0xAA;
-const uint8_t END_MARKER = 0x55;
-
-uint8_t checksum;
-uint8_t *bytes;
-
-// By default, compilers add padding bytes between struct members to optimize memory access speed.
-#pragma pack(push, 1) // Save the current alignment setting, then set alignment to 1 byte (no padding)
-struct PotSwitchValues
+// this should not exceed 64 bytes (RAWHID_TX_SIZE):
+#pragma pack(push, 1)
+struct HIDData
 {
-  uint16_t pot1 : bitdepth;
-  uint16_t pot2 : bitdepth;
-  uint16_t pot3 : bitdepth;
-  uint16_t pot4 : bitdepth;
-  bool switch1 : 1; // :4 for serialplot app
-  bool switch2 : 1; // :4 for serialplot app
-  bool switch3 : 1; // :4 for serialplot app // true == 1010
-};
-#pragma pack(pop) // Restore the previous alignment setting afterward.
+    uint16_t pots[numPots];                                                                              // 16 bytes
+    uint8_t buttons[numSwitchBytes];                                                                     // 1 byte (will later be more)
+    uint8_t padding[numPaddingBytes]; 
+} static report;
+#pragma pack(pop)
 
-PotSwitchValues Payload;
+// if struct size != 64, compilation fails with this Error:
+static_assert(sizeof(report) == RAWHID_TX_SIZE, "HIDPacket must be exactly 64 bytes");
 
 void setup()
 {
-  Serial.begin(115200);
-  while (!Serial)
-  { // wait for Serial...
-    delay(500);
-  }
+    Serial.begin(9600);
+    analogReadAveraging(1);
+    analogReadRes(bitdepth);
 
-  // set adc resolution to 14bits (= 16384 values)
-  analogReadResolution(bitdepth);
+    memset(&report, 0, sizeof(report)); // zero out the struct, so padding doesn't contain garbage
 
-  for (auto pin : switchPins)
-  {
-    pinMode(pin, INPUT_PULLDOWN);
-  }
-  for (auto pin : potPins)
-  {
-    pinMode(pin, INPUT);
-  }
+    for (auto pin : switchPins) pinMode(pin, INPUT_PULLDOWN);
+    for (auto pin : potPins) pinMode(pin, INPUT);
 }
 
 void readData()
 {
-  Payload.switch1 = digitalRead(switchPins[0]);
-  Payload.switch2 = digitalRead(switchPins[1]);
-  Payload.switch3 = digitalRead(switchPins[2]);
+    memset(&report, 0, sizeof(report)); // set all to 0
 
-  Payload.pot1 = analogRead(potPins[0]);
-  Payload.pot2 = analogRead(potPins[1]);
-  Payload.pot3 = analogRead(potPins[2]);
-  Payload.pot4 = analogRead(potPins[3]);
-}
+    for (int i = 0; i < numSwitches; i++)
+    {
+        report.buttons[i / 8] |= digitalRead(switchPins[i]) << (i % 8);
+    }
 
-void sendData()
-{
-  checksum = 0;
-  bytes = (uint8_t *)&Payload;
-  for (size_t i = 0; i < sizeof(Payload); i++)
-  {
-    checksum ^= bytes[i]; // XOR checksum
-  }
-
-  Serial.write(START_MARKER);
-  Serial.write(bytes, sizeof(Payload)); // send a buffer of bytes by giving a pointer (bytes) and a length (sizeof(Payload))
-  Serial.write(checksum);
-  Serial.write(END_MARKER); // skip maybe? 
-  // ..or get number of bytes, this is needed in supercollider
-  //Serial.println(sizeof(Payload));
+    for (int i = 0; i < numPots; i++)
+    {
+        report.pots[i] = analogRead(potPins[i]);
+    }
 }
 
 void loop()
 {
-  readData();
+    // uint32_t now = millis();
+    // report[0] = now / 100;
+    // uint8_t size = report[1];
+    // Serial.println(sizeof(report));
+    readData();
 
-  if (Serial.availableForWrite())
-  {
-    sendData();
-  }
-  // delay(tickrate);
+    // Serial.write((uint8_t*)&report, sizeof(report));
+    // report[1] = size;
+    // report[0] = 10;
+    //CustomHID.send((uint8_t *)&report, 1000);
+    RawHID.send((uint8_t *)&report, 1000);
+    delay(1);
 }
